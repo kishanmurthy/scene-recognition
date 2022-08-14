d84ec2f38f54023a952a4ce757505a446e08a3e7
Kishan Murthy <kishan.murth.n@gmail.com>
2022-08-13 19:35:10 -0700

    Updated the main Config to enable or disable TQDM

--- config/config.py
+++ config/config.py
@@ -1,5 +1,6 @@
 CONFIG = dict(
     DATASET_TRAIN_PATH = "/scratch1/knarasim/places365/places365_standard/train",
     DATASET_TEST_PATH = "/scratch1/knarasim/places365/places365_standard/val",
-    WANDB_PATH = "/scratch2/knarasim/places365test"
+    WANDB_PATH = "/scratch2/knarasim/places365",
+    DISABLE_TQDM = False
 )
