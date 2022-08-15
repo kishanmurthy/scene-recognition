e19d0c06fc6b9ce5721c804036ff802ba5478b22
Kishan Murthy <kishan.murth.n@gmail.com>
2022-08-15 16:53:41 -0700

    Fixed bug in evaluate.py

--- evaluate_test.py
+++ evaluate_test.py
@@ -22,7 +22,7 @@ from transformers import ViTForImageClassification,ViTFeatureExtractor
 from config import CONFIG
 from dataset import Places365
 from utils import ModelSaver, fetch_label_mappings, eval_one_epoch,collate_fn
-from utils import pretty_print_results, get_wandb_latest_run_id
+from utils import pretty_print_results
 DISABLE_TQDM= CONFIG['DISABLE_TQDM']
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 MODEL_NAME = 'google/vit-base-patch16-224-in21k'
@@ -49,12 +49,12 @@ def main():
     model = model.to(DEVICE)
 
     compute_metrics = {
-        'Accuracy Top 1': Accuracy().to(DEVICE),
-        'Accuracy Top 5': Accuracy(top_k=5).to(DEVICE),
+        'Accuracy Top 1': Accuracy(average='macro',num_classes=num_labels).to(DEVICE),
+        'Accuracy Top 5': Accuracy(average='macro',top_k=5,num_classes=num_labels).to(DEVICE),
         'Precision' : Precision(average='macro', num_classes=num_labels).to(DEVICE),
         'Recall' : Recall(average='macro', num_classes=num_labels).to(DEVICE),
-        'F1-Score': F1Score(num_classes=num_labels).to(DEVICE),
-        'AUROC': AUROC(num_classes=num_labels).to(DEVICE)
+        'F1-Score': F1Score(average='macro',num_classes=num_labels).to(DEVICE),
+        'AUROC': AUROC(average='macro', num_classes=num_labels).to(DEVICE)
     }
 
     test_loss, metrics = eval_one_epoch(model,test_loader,compute_metrics)
